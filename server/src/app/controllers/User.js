62ffb656bca2131f4fb6e2d0ee631ff85456dd43
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-06-03 22:17:24 -0300

    Controller Alternativo de Usuário

--- server/src/app/controllers/User.js
+++ server/src/app/controllers/User.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 import User from '../models/User';
 // import sendEmailRecovery from '../../'
 
@@ -7,11 +7,11 @@ class UserController {
     const user = req.payload;
 
     await User.findById(user.id)
-      .then((user) => {
-        if (!user)
+      .then((users) => {
+        if (!users)
           return res.status(401).json({ errors: 'User does not registred.' });
 
-        return res.json({ user: user.sendAuth() });
+        return res.json({ user: users.sendAuth() });
       })
       .catch(next);
   }
@@ -24,7 +24,7 @@ class UserController {
       .then((user) => {
         if (!user)
           return res.status(401).json({ errors: 'User does not registred.' });
-        console.log(user);
+
         return res.json({
           user: {
             name: user.name,
@@ -40,13 +40,18 @@ class UserController {
   async store(req, res, next) {
     const { name, email, password } = req.body;
 
+    if (!name || !email || !password)
+      return res.status(422).json({ errors: 'This fields are required.' });
+
     const user = await new User({ name, email });
     await user.setPassword(password);
 
     await user
       .save()
-      .then((_) => res.json({ user: user.sendAuth() }))
+      .then(() => res.json({ user: user.sendAuth() }))
       .catch(next);
+
+    return '';
   }
 
   async update(req, res, next) {
@@ -61,7 +66,7 @@ class UserController {
         if (typeof email !== 'undefined') user.email = email;
         if (typeof password !== 'undefined') user.setPassword(password);
 
-        return user.save().then((_) => {
+        return user.save().then(() => {
           return res.json({ user: user.sendAuth() });
         });
       })
@@ -78,7 +83,7 @@ class UserController {
 
         return user
           .remove()
-          .then((_) => {
+          .then(() => {
             return res.json({ deleted: true });
           })
           .catch(next);
@@ -109,6 +114,103 @@ class UserController {
       })
       .catch(next);
 
+    return '';
+  }
+
+  showRecobery(req, res) {
+    return res.render('recovery', { error: null, success: null });
+  }
+
+  async createRecovery(req, res, next) {
+    const { email } = req.body;
+
+    if (!email)
+      return res.render('recovery', {
+        error: 'Feed with your email',
+        success: null,
+      });
+
+    await User.findOne({ email })
+      .then((user) => {
+        if (!user)
+          return res.render('recovery', {
+            error: 'Does not exist user with this email',
+            success: null,
+          });
+
+        const recoveryData = user.recuperationToken();
+
+        return user
+          .save()
+          .then(() => {
+            return res.render('recovery', { error: null, success: true });
+          })
+          .catch(next);
+      })
+      .catch(next);
+    return '';
+  }
+
+  async showCompleteRecovery(req, res, next) {
+    if (!req.query.token)
+      return res.render('recovery', {
+        error: 'Token is not identified',
+        success: null,
+      });
+    await User.findOne({ 'recovery.token': req.query.token })
+      .then((user) => {
+        if (!user)
+          return res.render('recovery', {
+            error: 'Does not exist user with this email',
+            success: null,
+          });
+        if (new Date(user.recovery.date) < new Date())
+          return res.render('recovery', {
+            error: 'This token was expired.',
+            success: null,
+          });
+        return res.render('recovery/store', {
+          error: null,
+          success: null,
+          token: req.query.token,
+        });
+      })
+      .catch(next);
+
+    return '';
+  }
+
+  async completeRecovery(req, res, next) {
+    const { token, password } = req.body;
+    if (!token || !password)
+      return res.render('recovery/store', {
+        error: 'Please, try again with a new password',
+        success: null,
+        token,
+      });
+
+    await User.findOne({ 'recovery.token': token }).then((user) => {
+      if (!user)
+        return res.render('recovery', {
+          error: 'This user has not identified.',
+          success: null,
+        });
+
+      user.recuperationEnd();
+      user.setPassword(password);
+
+      return user
+        .save()
+        .then(() => {
+          return res.render('recovery/store', {
+            error: null,
+            success:
+              'The password has been succeeded to change. Try login again.',
+            token: null,
+          });
+        })
+        .catch(next);
+    });
     return '';
   }
 }
